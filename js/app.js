e1caa329a4e6ad518928f348c4910dc136205c26
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-30 15:23:12 +0200

    fonction aajout d'une personne dans l'annuaire

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
+$(document).ready(function() {
+
+	$('#submit').click(function() {
+		var annuaire = [];
+		var person = {	"nom":"",
+						"prenom":"",
+						"age":""};
+		person["nom"] = $("#inputFirstName").val();
+		person["prenom"] = $("#inputName").val();
+		person["age"] = $("#inputAge").val();
+		
+		annuaire.push(person);
+
+		$("#infoUn").html(person["prenom"]);
+		$("#infoDeux").html(person["nom"]);
+		$("#infoTrois").html(person["age"]);
+	});
+	
+});
+
+
 
 
