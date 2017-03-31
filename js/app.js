a34883359bda444bded21764f892367e104b4342
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-31 12:28:34 +0200

    Add function del

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	var annuaire = [];
+	var i;
 		$('#submit').on('click', function() {
 			var person = {	"nom":"",
 							"prenom":"",
@@ -13,16 +14,21 @@ $(document).ready(function() {
 			
 		$('input').val("");
 		$('#personneUn').html("");
-		for (var i = 0; i < annuaire.length; i++) {
+		for (i = 0; i < annuaire.length; i++) {
 			console.log(person);
 			var personne = annuaire[i];
-			$('#personneUn').append('<tr><td>'+ personne.nom+'</td>'+'<td>'+ personne.prenom+'</td>'+'<td>'+ personne.age+'</td></tr>');
+			$('#personneUn').append('<tr id="line'+i+'"><th>'+ personne.nom+'</th><th>'+ personne.prenom+'</th><th>'+ personne.age+'</th><th><button id="btn" type="button" class="btn btn-danger">X</button></th></tr>');
 			}
 			
 			//$('#personneUn').html('<td>'+ person["prenom"]+'</td>'+ '<td>'+ person["nom"]+'</td>'+'<td>'+ person["age"]+'</td>');
 			//return (annuaire[i]);};
 			
 		});
+
+	$('tbody').delegate('#btn', 'click', function() {
+		$(this).parent().parent().remove();
+		console.log('hello');
+	});
 			
 
 });
