52c8a350aae0cd1f05a7cb913f85921bcbdcdb8c
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-31 10:47:01 +0200

    fin fonction onclick

--- js/app.js
+++ js/app.js
@@ -1,24 +1,32 @@
 $(document).ready(function() {
-
-		var annuaire = [];
-	$('#submit').click(function() {
-		var person = {	"nom":"",
-						"prenom":"",
-						"age":""};
-		person["nom"] = $("#inputFirstName").val();
-		person["prenom"] = $("#inputName").val();
-		person["age"] = $("#inputAge").val();
+	var annuaire = [];
+		$('#submit').on('click', function() {
+			var person = {	"nom":"",
+							"prenom":"",
+							"age":""};
+			person["nom"] = $("#inputName").val();
+			person["prenom"] = $("#inputFirstName").val();
+			person["age"] = $("#inputAge").val();
 		
-		annuaire.push(person);
-		console.log(annuaire);
+			annuaire.push(person);
+
+			
 		$('input').val("");
-
-
-		$('#personneUn').html('<td>'+ person["prenom"]+'</td>'+ '<td>'+ person["nom"]+'</td>'+'<td>'+ person["age"]+'</td>');
-		
-	});
+		$('#personneUn').html("");
+		for (var i = 0; i < annuaire.length; i++) {
+			console.log(person);
+			var personne = annuaire[i];
+			$('#personneUn').append('<tr><td>'+ personne.nom+'</td>'+'<td>'+ personne.prenom+'</td>'+'<td>'+ personne.age+'</td></tr>');
+			}
+			
+			//$('#personneUn').html('<td>'+ person["prenom"]+'</td>'+ '<td>'+ person["nom"]+'</td>'+'<td>'+ person["age"]+'</td>');
+			//return (annuaire[i]);};
+			
+		});
+			
 
 });
+
 
 
 
